e23bf44ad4d3989708a13d430a2d7ad8a8eda9b1
krish1996sk <krish1996sk@gmail.com>
2018-08-13 20:00:32 +0000

    final

--- __init__.py
+++ __init__.py
@@ -1,17 +1,20 @@
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
 from mycroft.util.log import getLogger
+from adapt.intent import IntentBuilder
+
 
 LOGGER = getLogger(__name__)
 
+
 class Juniper_skill(MycroftSkill):
     def __init__(self):
-        super(WikipediaSkill, self).__init__(name="Juniper_skill")
+        super(Juniper_skill, self).__init__(name="Juniper_skill")
 
-    @intent_handler(IntentBuilder("Good_device").require("Query").require("juniper"))
+    @intent_handler(IntentBuilder("Good_device").require("Query").require("Juniper"))
     def handle_intent(self, message):
         # Extract what the user asked about
-        self.speak_dialog("good")
+        self.speak_dialog("Good")
 
 
 
